15bb955ac63518a49aeec42bea9e496e06932e30
Jesper Wøldiche <jesper@woeldiche.dk>
2011-10-24 23:07:42 +0200

    Styling of header and branding

--- sites/all/themes/fu2012/templates/region--branding.tpl.php
+++ sites/all/themes/fu2012/templates/region--branding.tpl.php
@@ -8,7 +8,12 @@
     <?php if ($site_name): ?>
       <?php $class = $site_name_hidden ? ' element-invisible' : ''; ?>
       <?php if ($is_front): ?>        
-        <h1 class="site-name<?php print $class; ?>"><?php print $linked_site_name; ?></h1>
+        <hgroup class="site-name<?php print $class; ?>">
+          <h1><?php print $linked_site_name; ?></h1>
+          <?php if ($site_slogan): ?>
+          <?php print $site_slogan; ?>
+          <?php endif; ?>
+        </hgroup>
       <?php else: ?>
         <h2 class="site-name<?php print $class; ?>"><?php print $linked_site_name; ?></h2>
       <?php endif; ?>
