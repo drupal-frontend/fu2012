c1dd19ff7c3de3cb7d6a04572c045ebce4c0fb01
Jesper Wøldiche <jesper@woeldiche.dk>
2011-11-10 13:20:54 +0100

    updated design

--- sites/all/themes/fu2012/templates/region--branding.tpl.php
+++ sites/all/themes/fu2012/templates/region--branding.tpl.php
@@ -7,16 +7,12 @@
     <?php endif; ?>
     <?php if ($site_name): ?>
       <?php $class = $site_name_hidden ? ' element-invisible' : ''; ?>
-      <?php if ($is_front): ?>        
-        <hgroup class="site-name<?php print $class; ?>">
-          <h1><?php print $linked_site_name; ?></h1>
-          <?php if ($site_slogan): ?>
-          <?php print $site_slogan; ?>
-          <?php endif; ?>
-        </hgroup>
-      <?php else: ?>
-        <h2 class="site-name<?php print $class; ?>"><?php print $linked_site_name; ?></h2>
-      <?php endif; ?>
+      <hgroup class="site-name<?php print $class; ?>">
+        <h1><?php print $linked_site_name; ?></h1>
+        <?php if ($site_slogan): ?>
+        <?php print $site_slogan; ?>
+        <?php endif; ?>
+      </hgroup>
     <?php endif; ?>
   <?php endif; ?>
   <?php print $content; ?>
--- sites/all/themes/fu2012/templates/zone--footer.tpl.php
+++ sites/all/themes/fu2012/templates/zone--footer.tpl.php
@@ -6,8 +6,6 @@
 ?>
 <?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>  
   <div<?php print $content_attributes; ?>>
-    <div class="block-secondary">
-      <?php print $content; ?>
-    </div>
+    <?php print $content; ?>
   </div>
 <?php if ($wrapper): ?></div><?php endif; ?>
